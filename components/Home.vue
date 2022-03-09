d9c7a43d5565ef83b336e1362ee284242a495cbd
trunkey2007 <hoquanglam090@gmail.com>
2022-03-09 20:35:12 +0700

    first commit

---
+++ components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-screen w-full">
+        <div class="py-5 px-36 font-bold">
+            Map
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <Header/>
+  <Nuxt />
+  <Footer/>
+</div>
+</template>
+
+<style>
+body {
+  font-family: "Overpass",-apple-system,BlinkMacSystemFont,Segoe UI,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif!important;
+  max-width: 100vw;
+  overflow-x: hidden;
+  background: rgb(255, 255, 255);
+}
+</style>
